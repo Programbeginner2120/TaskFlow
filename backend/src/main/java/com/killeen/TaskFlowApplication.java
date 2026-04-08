b74cf592d6f5fbc949567d69c745853d86d6483f
Matthew Killeen <Matthewkilleen99@gmail.com>
2026-04-08 15:03:08 -0400

    added exception.properties source

--- backend/src/main/java/com/killeen/TaskFlowApplication.java
+++ backend/src/main/java/com/killeen/TaskFlowApplication.java
@@ -2,8 +2,13 @@ package com.killeen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({
+	@PropertySource("classpath:exception.properties")
+})
 public class TaskFlowApplication {
 
 	public static void main(String[] args) {
