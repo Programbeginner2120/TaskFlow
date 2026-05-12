f9ad83d04662f50500254a6d3c7c5585c61fc159
Matthew Killeen <Matthewkilleen99@gmail.com>
2026-05-12 11:41:47 -0400

    regenerated mybatis entities

---
+++ backend/src/main/java/com/killeen/taskflow/db/model/generated/RefreshTokenDb.java
@@ -0,0 +1,65 @@
+package com.killeen.taskflow.db.model.generated;
+
+import java.time.OffsetDateTime;
+
+public class RefreshTokenDb {
+    private Long id;
+
+    private Long userId;
+
+    private String token;
+
+    private OffsetDateTime createdAt;
+
+    private OffsetDateTime expiresAt;
+
+    private OffsetDateTime usedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token == null ? null : token.trim();
+    }
+
+    public OffsetDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(OffsetDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public OffsetDateTime getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(OffsetDateTime expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+
+    public OffsetDateTime getUsedAt() {
+        return usedAt;
+    }
+
+    public void setUsedAt(OffsetDateTime usedAt) {
+        this.usedAt = usedAt;
+    }
+}
