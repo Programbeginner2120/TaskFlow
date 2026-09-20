f08578d1f751d5e61fb1c5330410cbcaa68cd26c
Matthew Killeen <Matthewkilleen99@gmail.com>
2026-09-20 17:40:40 -0400

    updated references to taskflow-qa

--- frontend/src/app/environments/environment.qa.ts
+++ frontend/src/app/environments/environment.qa.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    api_url: 'https://52.73.236.177.nip.io/api',
+    api_url: 'https://taskflow-qa.matthewrkilleen.com/api',
     environment_name: 'qa'
 };
